533fc684bd2451f6497c245df49283d8aaa1e60c
AnanthAkshay <ananthakshay2006@gmail.com>
2026-06-02 02:12:36 +0530

    Added Semester 4 notes portal and lab manuals

--- check_chars.py
+++
@@ -1,6 +0,0 @@
-with open('a:/SEM4_Complete/Java_Advanced_Lab_Book.html', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-for i, line in enumerate(lines):
-    if 'String sql = "INSERT INTO Representative VALUES' in line:
-        for char in line[-15:]:
-            print(f"char: {char} code: {ord(char)}")
